6f00c330694307bfc4cf534ea89dfc5611d51cd0
wangwei <510271184@qq.com>
2020-01-06 15:03:30 +0800

    新建controller

---
+++ src/main/java/com/breakyizhan/web/Controller/UserController.java
@@ -0,0 +1,22 @@
+/**
+ * FileName: UserController
+ * Author:   Administrator
+ * Date:     2020/1/6 15:03
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.breakyizhan.web.Controller;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2020/1/6
+ * @since 1.0.0
+ */
+public class UserController {
+
+}
