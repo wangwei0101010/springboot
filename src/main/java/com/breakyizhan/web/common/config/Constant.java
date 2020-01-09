8d0640a948ff6a43ef91953b53cd04db3fe81d96
wangwei <510271184@qq.com>
2020-01-09 16:02:34 +0800

    config

---
+++ src/main/java/com/breakyizhan/web/common/config/Constant.java
@@ -0,0 +1,12 @@
+package com.breakyizhan.web.common.config;
+
+/**
+
+ */
+public class Constant {
+    public static final String SNIPPET_DIR = "target/generated-snippets";
+    public static final String OUTPUT_DIR = "target/asciidoc";
+    public static final String URI = "/v2/api-docs";
+    public static final String FILE_NAME = "swagger.json";
+    public static final String FILE_PATH = OUTPUT_DIR + "/" + FILE_NAME;
+}
---
+++ src/main/java/com/breakyizhan/web/common/config/StreamTool.java
@@ -0,0 +1,41 @@
+package com.breakyizhan.web.common.config;
+
+import java.io.*;
+
+/**
+
+ */
+public class StreamTool {
+    public static byte[] read(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = inStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, len);
+        }
+        inStream.close();
+        return outputStream.toByteArray();
+    }
+
+    /**
+     * @param data     ：数据
+     * @param filePath ：要保存的文件路径，包含文件名及其后缀
+     * @author xiepuyao
+     */
+    public static void saveDataToFile(byte[] data, String filePath) throws IOException {
+
+        OutputStream os = null;
+        os = new FileOutputStream(new File(filePath));
+        os.write(data, 0, data.length);
+        os.flush();// 刷新缓冲区中的内容
+        os.close();// 关闭输出流
+    }
+
+    public static  void checkFile(String filePath) {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            file.mkdirs();
+            System.out.println("已创建目录:" + filePath);
+        }
+    }
+}
