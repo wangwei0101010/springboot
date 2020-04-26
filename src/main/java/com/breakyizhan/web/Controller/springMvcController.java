e910d806fcbec0eb946b37171cf8afdeced677d4
wangwei <510271184@qq.com>
2020-04-26 15:46:45 +0800

    create

---
+++ src/main/java/com/breakyizhan/web/Controller/springMvcController.java
@@ -0,0 +1,22 @@
+/**
+ * FileName: springMvcController
+ * Author:   Administrator
+ * Date:     2020/4/26 15:46
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.breakyizhan.web.Controller;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2020/4/26
+ * @since 1.0.0
+ */
+public class springMvcController {
+
+}
