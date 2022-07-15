99e763e09af4921f92bd26c550d0928d499f4d8b
wangwei <510271184@qq.com>
2022-07-15 11:23:26 +0800

    删除过期

--- src/main/java/com/breakyizhan/web/common/spring/Scheduler.java
+++ src/main/java/com/breakyizhan/web/common/spring/Scheduler.java
@@ -1,19 +1,11 @@
 package com.breakyizhan.web.common.spring;
 
-import com.alibaba.fastjson.JSONObject;
 import com.breakyizhan.web.common.redis.UserRedisUtils;
-import com.breakyizhan.web.common.util.CommonValue;
-import com.breakyizhan.web.model.GcxCountryNegativeList;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
 
 /**
  *<p>Title:Scheduler</p>
