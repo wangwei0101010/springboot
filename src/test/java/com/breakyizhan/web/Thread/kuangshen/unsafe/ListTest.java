a022887d406c525560595cc39e9f5c9334d43719
wangwei <510271184@qq.com>
2022-08-25 10:20:30 +0800

    线程安全的 list set map

---
+++ src/test/java/com/breakyizhan/web/Thread/kuangshen/unsafe/ListTest.java
@@ -0,0 +1,39 @@
+package com.breakyizhan.web.Thread.kuangshen.unsafe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+//ConcurrentModificationException 并发修改异常
+public class ListTest {
+
+  public static void main(String[] args) {
+    //并发下 ArrayList 不安全的
+    /**
+     * 解决方案:
+     * 1. List<String> list = new Vector<>();
+     * 2. Collections.synchronizedList(new ArrayList<>());
+     * 3. new CopyOnWriteArrayList<>();
+     *
+     * */
+    //CopyOnWriteArrayList 写入时复制 ,COW 计算机程序设计领域的一种优化策略
+    //多个线程调用的时候, list 读取的时候,是固定的,写入(覆盖)的时候,避免覆盖,造成数据问题
+    //CopyOnWriteArrayList比Vector区别 一个是sync(性能低)一个是lock锁
+    List<String> list =  new CopyOnWriteArrayList<>();
+    for (int i = 1; i <=10; i++) {
+      new Thread(()->{
+        list.add(UUID.randomUUID().toString().substring(0,5));
+        System.out.println(list);
+      },String.valueOf(i)).start();
+
+
+
+
+    }
+  }
+
+}
---
+++ src/test/java/com/breakyizhan/web/Thread/kuangshen/unsafe/SetTest.java
@@ -0,0 +1,31 @@
+package com.breakyizhan.web.Thread.kuangshen.unsafe;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+
+public class SetTest {
+
+  public static void main(String[] args) {
+//java.util.ConcurrentModificationException 并发修改异常
+    /**
+     * 解决方案:
+     * 1. Collections.synchronizedSet(new HashSet<>());
+     * 2. new CopyOnWriteArraySet<>();
+     *
+     * */
+    Set<String> set = new CopyOnWriteArraySet<>();
+    for (int i = 1; i <10 ; i++) {
+      new Thread(()->{
+        set.add(UUID.randomUUID().toString().substring(0,5));
+        System.out.println(set);
+      },String.valueOf(i)).start();
+
+    }
+
+  }
+
+}
