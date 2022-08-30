5c85d8d060a5f7d11d00174a066c9c263d48c219
wangwei <510271184@qq.com>
2022-08-30 16:10:33 +0800

    Stream流计算

---
+++ src/test/java/com/breakyizhan/web/Thread/kuangshen/Stream/UserStream.java
@@ -0,0 +1,16 @@
+package com.breakyizhan.web.Thread.kuangshen.Stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserStream {
+
+  private int id;
+  private String name;
+  private int age;
+
+}
